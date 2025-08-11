0a44c2fbedbb35ab3e58e14de071b7bf3099f8cc
xitalianx <106681530+xitalianx@users.noreply.github.com>
2025-08-11 15:53:29 +0200

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -78,14 +78,11 @@ const Hero = () => {
             />
           </div>
         </div>
-        {/* icon */}
-        <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce'>
-          <RiArrowDownSLine className='text-3xl text-primary' />
-        </div>
       </div>
     </section>
   );
 };
 
 export default Hero;
+
 
